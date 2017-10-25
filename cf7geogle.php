521e7edcfe40a093351b8e90eef747738a41f173
shibata <shibata@jidaikobo.com>
2017-10-25 15:21:56 +0900

    add licence

--- cf7geogle.php
+++ cf7geogle.php
@@ -3,11 +3,26 @@
 Plugin Name: cf7geogle
 Plugin URI: (プラグインの説明と更新を示すページの URI)
 Description: Google Map plugin for Contact Form 7
-Version: (プラグインのバージョン番号。例: 1.0)
-Author: (プラグイン作者の名前)
-Author URI: (プラグイン作者の URI)
-License: (ライセンス名の「スラッグ」 例: GPL2)
- */
+Version: 1.0
+Author: konagai[at]jidaikobo.com
+Author URI: http://www.jidaikobo.com
+License: GPL2
+
+Copyright 2017 jidaikobo (email : support@jidaikobo.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 
 /**
